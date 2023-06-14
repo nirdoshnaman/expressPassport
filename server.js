0e1eab6df8c4c4a1083d047f5250c887630ef5d2
nirdosh-mtalkz <nirdosh.mtalkz@gmail.com>
2023-06-14 14:40:10 +0530

    creating basic routes

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const exports = require('express')
+const express = require('express')
 const app = express()
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'Naman'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs', {name: 'Naman'})
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs', {name: 'Naman'})
+})
+
 
 app.listen(5000)
